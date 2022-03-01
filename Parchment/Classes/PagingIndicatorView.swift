544e21b630fbe2e858076a32beb89bad9b3be3d5
Noor <muhammadnoor007@gmail.com>
2022-03-01 16:06:05 +0700

    Custom selected tab

--- Parchment/Classes/PagingIndicatorView.swift
+++ Parchment/Classes/PagingIndicatorView.swift
@@ -5,10 +5,26 @@ import UIKit
 /// this type if you need further customization; just override the
 /// `indicatorClass` property in `PagingViewController`.
 open class PagingIndicatorView: UICollectionReusableView {
-    open override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
-        super.apply(layoutAttributes)
-        if let attributes = layoutAttributes as? PagingIndicatorLayoutAttributes {
-            backgroundColor = attributes.backgroundColor
-        }
+  
+  override open func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+    super.apply(layoutAttributes)
+    if let attributes = layoutAttributes as? PagingIndicatorLayoutAttributes {
+        backgroundColor = .clear
+        
+        let centerView = UIView()
+        centerView.backgroundColor = attributes.backgroundColor
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        centerView.layer.cornerRadius = frame.height / 2
+        addSubview(centerView)
+        
+        NSLayoutConstraint.activate([
+            centerView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            centerView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            centerView.heightAnchor.constraint(equalToConstant: frame.height),
+            centerView.widthAnchor.constraint(equalToConstant: 18)
+        ])
+        layoutIfNeeded()
     }
+  }
+  
 }
